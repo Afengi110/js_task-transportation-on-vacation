9a45b04af31241a35d3d845b6240a21d4933efc4
Afengi <gorodny.ivan4@gmail.com>
2025-08-21 21:34:50 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RATE = 40;
+  const MEDIUM_TERM_THRESHOLD = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const LONG_TERM_THRESHOLD = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const totalCost = DAILY_RATE * days;
+
+  if (days >= MEDIUM_TERM_THRESHOLD && days < LONG_TERM_THRESHOLD) {
+    return totalCost - MEDIUM_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM_THRESHOLD) {
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
